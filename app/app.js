b434ab2cccacf044dccd18fb893b0ec39a274b99
hangju0610 <phj950610@naver.com>
2023-05-27 14:16:05 +0900

    프런트단 JS 파일 만들기

--- app/app.js
+++ app/app.js
@@ -12,6 +12,8 @@ app.set("views","./src/views");
 app.set("view engine","ejs");
 
 app.use('/', home); // use -> 미들 웨어를 등록해주는 메서드.
+app.use(express.static(`${__dirname}/src/public`)); // 프론트 JS를 위한 미들웨어, 정적 경로를 추가한다.
+
 
 // app 내보내기
 module.exports = app;
