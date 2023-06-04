f6b500ea11bd429d4d70589b624aa450f90ef7bd
hangju0610 <phj950610@naver.com>
2023-06-04 16:27:23 +0900

    로그 관리 모듈 winston 등록 및 설정

---
+++ app/src/config/logger.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const {createLogger, transports, format } = require("winston");
+const {combine, timestamp, json, simple, colorize, printf, label} = format;
+
+const printFormat = printf(({timestamp,label, message, level}) => {
+   return `${timestamp} [${label}] ${level} : ${message}`;
+})
+
+const printLogFormat = {
+   file : combine(
+   label({
+      label : "백엔드 맛보기"
+   }),
+   timestamp({
+      format: "YYYY-MM-DD HH:mm:dd"
+   }),
+   printFormat
+   ),
+   consloe : combine(
+      colorize(),
+      simple()
+   ),
+};
+
+const opts = {
+   file : new transports.File({
+      filename : "access.log",
+      dirname : "./logs",
+      level: "info",
+      format: printLogFormat.file,
+   }),
+   console : new transports.Console({
+      level: "info",
+      format: printLogFormat.consloe,
+   }),
+}
+
+
+const logger = createLogger({
+   transports: [opts.file],
+})
+
+
+// 개발 중인 파일과, 실제 만들어진것을 구분
+if (process.env.NODE_ENV !== "production") {
+   logger.add(opts.console)
+}
+
+module.exports = logger;
